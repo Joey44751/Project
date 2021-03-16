16e7e55604bec6f3f128e4084238c137e7e3ebdc
Joey44751 <joey44751@gmail.com>
2021-03-16 17:06:24 +0100

    added http post/get/update/delete integration

---
+++ src/app/http.model.ts
@@ -0,0 +1,8 @@
+export class Playground {
+    constructor(
+        public id: number,
+        public name: string,
+        public status:boolean
+    )
+    {}
+}
---
+++ src/app/http.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Playground } from './http.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class PlaygroundService {
+constructor(private http: HttpClient){}
+url:string = "https://syntra.terugsamen.be/mightyducks/public/api/companies";
+
+    getPlayground(): Observable<Playground[]> {
+        return this.http
+            .get<Playground[]>(this.url)
+            .pipe(
+                tap (result => console.log('Via onze eigen API:', result))
+            )
+    }
+    getPlaygroundById(id:number) {
+        console.log(id);
+        return this.http
+        .get<Playground>(this.url+'/'+id)
+        .pipe()
+    }
+
+    addPlayground(newPlayground:Playground): Observable<any> {
+        const headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.post(this.url,newPlayground,{headers:headers});
+    }
+
+    updatePlayground(newPlayground:Playground): Observable<any> {
+        const headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.put(this.url+'/'+newPlayground.id,newPlayground, {headers:headers});
+    }
+
+
+    deletePlayground(id: number){
+        return this.http.delete(this.url+'/'+id);
+    }
+    
+}
