d5bedf72888ced26bdd840cdf1ed9ada2c3bcb8f
Joey44751 <joey44751@gmail.com>
2021-03-16 17:11:37 +0100

    Update http model

--- src/app/http.model.ts
+++ src/app/http.model.ts
@@ -2,7 +2,17 @@ export class Playground {
     constructor(
         public id: number,
         public name: string,
-        public status:boolean
+        public address:string,
+        public email:string, 
+        public phone:string, 
+        public vat:number,
+        public site:string, 
+        public category_id:number,
+        public hours:number,
+        public dates:Date,
+        public prices:number,
+        public extras:string
+
     )
     {}
 }
