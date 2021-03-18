5326793d3d03c51b034545e4e88bdfb1c6c8a053
Joey44751 <joey44751@gmail.com>
2021-03-18 14:36:18 +0100

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit {
   lat:any;
   lng:any;
   
+<<<<<<< HEAD
   constructor(public authService: AuthService) {
     if (navigator){
       navigator.geolocation.getCurrentPosition(pos=>{
@@ -22,31 +23,25 @@ export class HomeComponent implements OnInit {
       })
     }
   }
+=======
+  constructor() {}
+>>>>>>> 31be46eb18f2eac1a485fbaa6a534a97d351937d
 
     center = ({lat: 5.03076, lng: 5.03076});
     // set center(center: google.maps.LatLngLiteral | google.maps.LatLng);
   
 
   ngOnInit(): void {
-   
+    // get user location form browser or cell phone GPS
     navigator.geolocation.getCurrentPosition((p) =>{
+      // set center of map to geolocation coordinates
       this.center = {
         lat: p.coords.latitude,
         lng: p.coords.longitude
         }
       console.log(this.center);
       })
-    
-   
- };
+    };
   
-
-      
-   
-   
-   
-
-
-
 }
 
